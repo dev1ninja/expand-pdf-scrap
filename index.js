29a6811ace7cab125dcdfaec01a843cc8662c802
dev1ninja <devninja.up@gmail.com>
2021-09-19 20:01:55 -0700

    update

--- index.js
+++ index.js
@@ -1,24 +1,45 @@
 const fs = require('fs');
-const axios = require('axios');
+const axios = require('axios').default;
+const axiosCookieJarSupport = require('axios-cookiejar-support').default
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const tough = require('tough-cookie');
+
+const virtualConsole = new jsdom.VirtualConsole();
+
+//axiosCookieJarSupport(axios);
+const cookieJar = new tough.CookieJar();
+axios.defaults.jar = cookieJar
 
 const pdfUrl = 'https://cma.org.sa/Market/imf/Pages/default.aspx';
-
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
+/*
 axios.get(pdfUrl)
 .then(function (response) {
-  const dom = new JSDOM(response.data, { runScripts: "dangerously", resources: "usable", pretendToBeVisual: true });
-  console.log(dom.window.document.querySelector('title').textContent);
-  let a = [...dom.window.document.querySelectorAll('a')];
-  let osList = [...dom.window.document.getElementsByClassName('os')];
-  let archList = [...dom.window.document.querySelectorAll('.arch')];
-  let downloadLinks = [...dom.window.document.querySelectorAll('.main-download__variant__sub-table__download')];
-  console.log(a);
-  console.log(osList);
-  console.log(archList);
-  console.log(downloadLinks);
+  const dom = new JSDOM(response.data, { pretendToBeVisual: true, resources: 'usable' });
+  //fs.writeFileSync('body.txt', dom.window.document);
+  //console.log("this is dom", dom.window.document.querySelector('a').textContent)
+  dom.window.document.querySelectorAll('a').forEach(link => {
+    console.log(link.href);
+  });
+  console.log("DOM: ", dom)
 })
 .catch(function (error) {
-  console.log(error);
+  console.log('error : ',error);
 });
+*/
+async function loadListView(){
+    const dom = await JSDOM.fromURL(pdfUrl, {
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+        includeNodeLocations: true,
+        storageQuota: 10000000,
+        runScripts: 'dangerously',
+        resources: "usable",
+        virtualConsole,
+        cookieJar,
+    });
+    dom.window.document.querySelectorAll('a').forEach(link => {
+    	console.log(link.href);
+  	});
+    return dom;
+}
+loadListView()
