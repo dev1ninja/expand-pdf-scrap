0dea0f8e1b2eb92b7b9896c2ae18b4c853b21341
dev1ninja <devninja.up@gmail.com>
2021-09-19 17:58:49 -0700

    update

--- index.js
+++ index.js
@@ -3,5 +3,22 @@ const axios = require('axios');
 
 const pdfUrl = 'https://cma.org.sa/Market/imf/Pages/default.aspx';
 
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 axios.get(pdfUrl)
-    .then(({ data }) => fs.writeFileSync('body.txt', data));
+.then(function (response) {
+  const dom = new JSDOM(response.data, { runScripts: "dangerously", resources: "usable", pretendToBeVisual: true });
+  console.log(dom.window.document.querySelector('title').textContent);
+  let a = [...dom.window.document.querySelectorAll('a')];
+  let osList = [...dom.window.document.getElementsByClassName('os')];
+  let archList = [...dom.window.document.querySelectorAll('.arch')];
+  let downloadLinks = [...dom.window.document.querySelectorAll('.main-download__variant__sub-table__download')];
+  console.log(a);
+  console.log(osList);
+  console.log(archList);
+  console.log(downloadLinks);
+})
+.catch(function (error) {
+  console.log(error);
+});
