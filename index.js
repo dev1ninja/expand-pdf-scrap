f9d6e49cf950c0f32435b3371f5c029385caa995
dev1ninja <devninja.up@gmail.com>
2021-09-19 17:57:38 -0700

    update

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
 const fs = require('fs');
-const jsdom = require('jsdom');
-const got = require('got');
-const { JSDOM } = jsdom;
-const fetch = require('node-fetch');
-
+const axios = require('axios');
 
 const pdfUrl = 'https://cma.org.sa/Market/imf/Pages/default.aspx';
 
-(async () => {
-  let htmlDoc = await fetch(pdfUrl)
-    .then((res) => res.text())
-    .then((body) => body); 
-  try {
-    const document = new jsdom.JSDOM().window.document;
-    fs.writeFileSync('body.txt', htmlDoc);
-
-  } catch (e) {
-    console.log('error', e);
-  }
- })(); 
+axios.get(pdfUrl)
+    .then(({ data }) => fs.writeFileSync('body.txt', data));
